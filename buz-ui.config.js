f070f38b6cd0626bce8f42b6c7e8638fab89ac77
wangruipeng <smalnew@163.com>
2019-02-20 11:30:58 +0800

    添加windows 打包ui

--- buz-ui.config.js
+++ buz-ui.config.js
@@ -41,7 +41,8 @@ function createModuleIdFactory() {
     }
     name = name.replace('.js','');
     name = name.replace('.png','');
-    name = name.replace(new RegExp(pathSep,"gm"),'_');
+    let regExp = pathSep=='\\'?new RegExp('\\\\',"gm"):new RegExp(pathSep,"gm");
+    name = name.replace(regExp,'_');//把path中的/换成下划线
     return name;
   };
 }
